ff1e6e28ca1c5fd10b394b8678b19553742eb270
Alyoshka <alyoshkamyshko@gmail.com>
2023-10-20 14:35:15 -0700

    Create transaction controller

---
+++ server/controllers/transactionController.js
@@ -0,0 +1,19 @@
+const asyncHandler = require('express-async-handler');
+
+const Transaction = require('../models/transaction');
+
+exports.list = asyncHandler(async (req, res, next) => {
+    res.json({})
+});
+
+exports.create = asyncHandler(async (req, res, next) => {
+    res.json({})
+});
+
+exports.update = asyncHandler(async (req, res, next) => {
+    res.json({})
+});
+
+exports.delete = asyncHandler(async (req, res, next) => {
+    res.json({})
+});
