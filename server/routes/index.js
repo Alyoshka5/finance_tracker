7f13bb5458b0d9f18cf4677b36fee7f821688363
Alyoshka <alyoshkamyshko@gmail.com>
2023-10-20 14:48:54 -0700

    Add transaction routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const transactionController = require('../controllers/transactionController')
+
+
+// transactions
+router.get('/transactions', transactionController.list);
+
+router.post('/transaction/create', transactionController.create);
+
+router.put('/transaction/:id/update', transactionController.update);
+
+router.delete('/transaction/:id/delete', transactionController.delete);
+
 
 module.exports = router;
