388d4c68d79b55b0f9b36ae7da5d490c94d22cb0
Alyoshka <alyoshkamyshko@gmail.com>
2023-10-25 22:25:20 -0700

    Respond with transaction data in  list method

--- server/controllers/transactionController.js
+++ server/controllers/transactionController.js
@@ -3,7 +3,9 @@ const asyncHandler = require('express-async-handler');
 const Transaction = require('../models/transaction');
 
 exports.list = asyncHandler(async (req, res, next) => {
-    res.json({})
+    const transactions = await Transaction.find({}).exec();
+
+    res.json(transactions);
 });
 
 exports.create = asyncHandler(async (req, res, next) => {
