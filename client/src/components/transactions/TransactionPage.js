a1687946cdeef4eb03d7a7461eb0bf317217d5ad
Alyoshka <alyoshkamyshko@gmail.com>
2023-10-25 22:21:45 -0700

    Add fetch request to get transactions from database

--- client/src/components/transactions/TransactionPage.js
+++ client/src/components/transactions/TransactionPage.js
@@ -1,5 +1,19 @@
+import { useState, useEffect } from 'react';
 
 export default function TransactionPage() {
+
+    const [transactions, setTransactions] = useState([]);
+
+    const getTransactions = async () => {
+        const response = await fetch('/transactions');
+        const transactionsData = await response.json();
+
+        setTransactions(transactionsData);
+    }
+
+    useEffect(() => {
+        getTransactions();
+    }, []);
 
     return (
         <div>
