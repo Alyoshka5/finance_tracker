f4d60bc693ad324ad6e5f61a2ef1e3ee2760738c
Alyoshka <alyoshkamyshko@gmail.com>
2023-11-15 20:15:56 -0800

    Add cors middleware

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 require('dotenv').config();
 
@@ -20,15 +21,12 @@ async function main() {
   await mongoose.connect(mongoDbUrl);
 };
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(cors());
 
 app.use('/', indexRouter);
 
