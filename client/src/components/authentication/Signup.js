18f45e3e925d31213b76f8e94a650ce8d2fa4273
Alyoshka <alyoshkamyshko@gmail.com>
2023-11-27 21:31:29 -0800

    feat: add account creation form to signup page

--- client/src/components/authentication/Signup.js
+++ client/src/components/authentication/Signup.js
@@ -1,5 +1,110 @@
+import { useState, useEffect } from 'react';
+import { TextField, Box, Grid, Container, Button, Typography } from '@mui/material';
 
+const emailRegex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-]+)(\.[a-zA-Z]{2,5}){1,2}$/;
+const pwdRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
 export default function Signup() {
-    
+    const [email, setEmail] = useState('');
+    const [validEmail, setValidEmail] = useState(false);
+    const [emailFocus, setEmailFocus] = useState(false);
+
+    const [pwd, setPwd] = useState('');
+    const [validPwd, setValidPwd] = useState(false);
+    const [pwdFocus, setPwdFocus] = useState(false);
+
+    const [matchPwd, setMatchPwd] = useState('');
+    const [validMatch, setValidMatch] = useState(false);
+    const [matchFocus, setMatchFocus] = useState(false);
+
+    useEffect(() => {
+        setValidEmail(emailRegex.test(email));
+    }, [email]);
+
+    useEffect(() => {
+        const result = pwdRegex.test(pwd);
+        setValidPwd(result);
+        const match = pwd === matchPwd;
+        setValidMatch(match);
+    }, [pwd, matchPwd]);
+
+    return (
+        <Container component='main' maxWidth='xs'>
+            <Box component='form' noValidate
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}
+            >
+                <Typography component='h1' variant='h4'>
+                    Create Account
+                </Typography>
+
+                <Grid container
+                    spacing={2}
+                    sx={{
+                        mt: 2,
+                        display: 'flex',
+                        justifyContent: 'center'
+                }}>
+                    <Grid item xs={12}>
+                        <TextField
+                            required
+                            fullWidth
+                            autoFocus
+                            name='email'
+                            label='Email Address'
+                            autoComplete='email'
+                            error={email !== '' && !validEmail && !emailFocus}
+                            helperText={(email && !validEmail && !emailFocus) ? 'Invalid Email' : ''}
+                            onFocus={() => setEmailFocus(true)}
+                            onBlur={() => setEmailFocus(false)}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            required
+                            fullWidth
+                            name='password'
+                            label='Password'
+                            type='password'
+                            autoComplete='new-password'
+                            error={pwd !== '' && !validPwd && !pwdFocus}
+                            helperText={(pwd && !validPwd && !pwdFocus) ? 'Must include an uppercase and lowercase letter, a number, and a special character' : ''}
+                            onFocus={() => setPwdFocus(true)}
+                            onBlur={() => setPwdFocus(false)}
+                            onChange={(e) => setPwd(e.target.value)}
+                            
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            required
+                            fullWidth
+                            name='confirm-password'
+                            label='Confirm Password'
+                            type='password'
+                            error={matchPwd !== '' && !validMatch && !matchFocus}
+                            helperText={(matchPwd && !validMatch && !matchFocus) ? 'Passwords must match' : ''}
+                            onFocus={() => setMatchFocus(true)}
+                            onBlur={() => setMatchFocus(false)}
+                            onChange={(e) => setMatchPwd(e.target.value)}
+                        />
+                    </Grid>
+                    <Grid item>
+                        <Button
+                            type='submit'
+                            variant='contained'
+                            disabled={!(validEmail && validPwd && validMatch)}
+                        >
+                            Create Account
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Box>
+        </Container>
+    )
 }
