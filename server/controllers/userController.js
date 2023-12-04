28ce7aa644f61330def037541793e8448f613de8
Alyoshka <alyoshkamyshko@gmail.com>
2023-12-03 22:34:19 -0800

    feat: add login function
    The login function currently authenticates a user, but does not keep
    the user logged in. JWT access and refresh tokens will later be sent
    back to the client when a user is authenticated to allow the client to
    make subsequent requests to the server.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -29,6 +29,19 @@ exports.signup = asyncHandler(async (req, res, next) => {
 
 });
 
-exports.login = asyncHandler((req, res, next) => {
-    res.json({});
+exports.login = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password)
+        return res.status(400).json({ 'message': 'Email and password are required'});
+
+    const user = await User.findOne({email: email}).exec();
+    if (!user)
+        return res.status(401).json({ 'message': 'Email not found.'});
+    
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (passwordMatch)
+        res.status(200).json({ 'message': 'Loged in' });
+    else
+        res.status(401).json({ 'message': 'Incorrect email or password' });
 });
