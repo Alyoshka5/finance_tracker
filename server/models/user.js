57808c14268c3abbfde679782478a2c58510b080
Alyoshka <alyoshkamyshko@gmail.com>
2023-12-06 22:09:38 -0800

    feat(schema): add refreshToken field
    The refreshToken field will be used keep track of the user's jwt refresh
    token. This token can be used to generate new access tokens and can be
    terminated if the user logs out

--- server/models/user.js
+++ server/models/user.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
     email: { type: String, required: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    refreshToken: { type: String }
 });
 
 module.exports = mongoose.model('User', UserSchema);
