18dc6976b3eaec4d769e3c7d7d707cf7c465513f
Alyoshka <alyoshkamyshko@gmail.com>
2023-12-06 22:15:03 -0800

    feat(auth): add function to verify jwt access token
    This function will be used as middleware and will be called anytime a
    user tries access a non-auth route. This will prevent non-logged-in
    users from accessing authentication reliant routes such as transactions.

---
+++ server/middleware/verifyJWT.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+
+    
+    if (!authHeader)
+        return res.status(401).json({ message: 'No token provided' });
+    const token = authHeader.split(' ')[1];
+    
+    console.log(token);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err)
+            return res.status(403).json({ message: 'Failed to authenticate token' });
+
+        req.userId = decoded.userId;
+        next();
+    });
+}
+
+module.exports = verifyJWT;
