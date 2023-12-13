cdb9151699231509a61ec280a6fb5f7da5782a15
Alyoshka <alyoshkamyshko@gmail.com>
2023-12-12 22:00:33 -0800

    feat(auth): send authorization header on request
    The authorization header is required since the transactions route is
    protected on the server and requires a jwt access token.

--- client/src/components/transactions/TransactionPage.js
+++ client/src/components/transactions/TransactionPage.js
@@ -1,13 +1,19 @@
 import { useState, useEffect } from 'react';
 import TransactionEntry from './TransactionEntry';
 import axios from 'axios';
+import useAuth from '../../hooks/useAuth';
 
 export default function TransactionPage() {
+    const { auth } = useAuth();
 
     const [transactions, setTransactions] = useState([]);
 
     const getTransactions = async () => {
-        const response = await axios.get('/transactions');
+        const response = await axios.get('/transactions', {
+            headers: {
+                'authorization': `Bearer ${auth.accessToken}`
+            }
+        });
 
         setTransactions(response.data);
     }
