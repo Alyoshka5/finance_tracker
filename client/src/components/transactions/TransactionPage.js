f4ff0a3d549015fb053cc197e230a5a8df28dafb
Alyoshka <alyoshkamyshko@gmail.com>
2023-12-22 09:44:33 -0800

    feat(auth): add logout link and handler

--- client/src/components/transactions/TransactionPage.js
+++ client/src/components/transactions/TransactionPage.js
@@ -2,11 +2,13 @@ import { useState, useEffect } from 'react';
 import TransactionEntry from './TransactionEntry';
 import useAxiosPrivate from '../../hooks/useAxiosPrivate';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
+import useLogout from '../../hooks/useLogout';
 
 export default function TransactionPage() {
     const axiosPrivate = useAxiosPrivate();
     const navigate = useNavigate();
     const location = useLocation();
+    const logout = useLogout();
 
     const [transactions, setTransactions] = useState([]);
 
@@ -19,12 +21,18 @@ export default function TransactionPage() {
         }
     }
 
+    const handleLogout = async () => {
+        await logout();
+        navigate('/login');
+    }
+
     useEffect(() => {
         getTransactions();
     }, []);
 
     return (
         <div>
+            <a href='' onClick={handleLogout}>Logout</a>
             <h1>Transactions</h1>
             {transactions.map(transaction => (
                 <TransactionEntry key={transaction.date} transaction={transaction} />
