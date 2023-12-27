9fe0717b6cf5071132b94f6d82a1a27b70236ff2
Alyoshka <alyoshkamyshko@gmail.com>
2023-12-26 20:20:40 -0800

    feat: change component to use modal context states

--- client/src/components/ModalContainer.js
+++ client/src/components/ModalContainer.js
@@ -1,27 +1,31 @@
 import { Box } from '@mui/material';
-import '../App.css';
+import { Outlet } from 'react-router-dom';
+import useModal from '../hooks/useModal';
 
-export default function ModalContainer({ children, open, setOpen }) {
+export default function ModalContainer() {
+    const {modalOpen, setModalOpen, contentComponent} = useModal();
 
     return (
-        <Box
-        sx={{
-                display: (open ? 'flex' : 'none'),
-                width: '100vw',
-                height: '100vh',
-                position: 'absolute',
-                top: '0',
-                left: '0',
-                justifyContent: 'center',
-                alignItems: 'center',
-                backgroundColor: '#ffffff00',
-                backdropFilter: 'blur(3px)',
-                
-            }}
-            className={`modal ${open ? 'active' : ''}`}
-            onClick={(e) => (e.target === e.currentTarget) && setOpen(false)}
-        >
-            {children}
+        <Box>
+            <Box
+            sx={{
+                    display: (modalOpen ? 'flex' : 'none'),
+                    width: '100vw',
+                    height: '100vh',
+                    position: 'absolute',
+                    top: '0',
+                    left: '0',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundColor: '#ffffff00',
+                    backdropFilter: 'blur(3px)',
+                    
+                }}
+                onClick={(e) => (e.target === e.currentTarget) && setModalOpen(false)}
+                >
+                {contentComponent}
+            </Box>
+            <Outlet />
         </Box>
     )
 }
