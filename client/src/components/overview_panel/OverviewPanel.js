b657c0b6b60feaca01531b36a89f09584a79609f
Alyoshka <alyoshkamyshko@gmail.com>
2023-12-30 23:03:46 -0800

    feat: create overview panel component

---
+++ client/src/components/overview_panel/OverviewPanel.js
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+
+export default function OverviewPanel() {
+
+    return (
+        <Box>
+            <Typography variant='h5'>Overview</Typography>
+        </Box>
+    )
+}
