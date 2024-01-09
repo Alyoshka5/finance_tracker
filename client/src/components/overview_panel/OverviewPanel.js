917f5c814a785ce56dfd11ad2bff545511c836e4
Alyoshka <alyoshkamyshko@gmail.com>
2024-01-08 21:09:34 -0800

    feat(overview-panel): add styling to type buttons
    Displayed button container in flex and added brighter opacity and
    underline on active type button while reducing opacity of the other
    button.

--- client/src/components/overview_panel/OverviewPanel.js
+++ client/src/components/overview_panel/OverviewPanel.js
@@ -67,9 +67,29 @@ export default function OverviewPanel() {
 
             <OverviewChart groupList={groupList} />
 
-            <Box>
-                <Button variant={transactionType === 'Expense' ? 'outlined' : ''} onClick={() => setTransactionType('Expense')}>Expense</Button>
-                <Button variant={transactionType === 'Income' ? 'outlined' : ''} onClick={() => setTransactionType('Income')}>Income</Button>
+            <Box sx={{
+                display: 'flex',
+                gap: '1rem',
+                marginBottom: '1rem'
+            }}>
+                <Button
+                    sx={{
+                        borderBottom: transactionType === 'Expense' ? `2px solid #b2d5eeaa` : '',
+                        opacity: transactionType === 'Expense' ? `1` : '0.7',
+                        borderRadius: '0',
+                        padding: '0',
+                    }}
+                    onClick={() => setTransactionType('Expense')}
+                >Expense</Button>
+                <Button
+                    sx={{
+                        borderBottom: transactionType === 'Income' ? `2px solid #b2d5eeaa` : '',
+                        opacity: transactionType === 'Income' ? `1` : '0.7',
+                        borderRadius: '0',
+                        padding: '0',
+                    }}
+                    onClick={() => setTransactionType('Income')}
+                >Income</Button>
             </Box>
 
             {Object.keys(groupList).map(key => {
