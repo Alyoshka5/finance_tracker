6aa74b3c9f320bc6d333d3ccd1ae04dd64198ea4
Alyoshka <alyoshkamyshko@gmail.com>
2024-01-11 20:06:04 -0800

    feat(layout): change layout of components

--- client/src/components/transactions/TransactionTableContainer.js
+++ client/src/components/transactions/TransactionTableContainer.js
@@ -7,10 +7,21 @@ export default function TransactionTableContainer() {
     const openModal = useOpenModal();
 
     return (
-        <Box>
-            <Typography variant='h4'>Transactions</Typography>
+        <Box
+            display='flex'
+            flexDirection='column'
+            gap='0.5rem'
+            padding='1rem 1.2rem'
+        >
+            <Box
+                display='flex'
+                justifyContent='space-between'
+            >
+                <Typography variant='h4'>Transactions</Typography>
             <TransactionTable />
-            <Button variant='outlined' onClick={() => openModal(<TransactionForm />)}>Add Transaction</Button>
+                <Button variant='outlined' onClick={() => openModal(<TransactionForm />)}>Add Transaction</Button>
+            </Box>
+            <TransactionTable />
         </Box>
     )
 }
